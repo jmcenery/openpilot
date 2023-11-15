17a7f50f2b156354d2bfd46548b6468014cb35db
jmcenery <33725231+jmcenery@users.noreply.github.com>
2023-11-15 14:02:44 -0500

    Update interface.py

--- selfdrive/car/mazda/interface.py
+++ selfdrive/car/mazda/interface.py
@@ -16,7 +16,7 @@ class CarInterface(CarInterfaceBase):
     ret.safetyConfigs = [get_safety_config(car.CarParams.SafetyModel.mazda)]
     ret.radarUnavailable = True
 
-    ret.dashcamOnly = False
+    ret.dashcamOnly = False 
 
     ret.steerActuatorDelay = 0.1
     ret.steerLimitTimer = 0.8
